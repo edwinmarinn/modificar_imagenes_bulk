696af17de0403b4a3d7e1bce7821f1e8e515edf0
Edwin <edwin.marin@devimed.com.co>
2021-08-01 12:39:29 -0500

    Deep search

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
 
     IMAGES_PATH  = BASE_PATH / "fotos"
     IMAGES_OUT_PATH = BASE_PATH / "salida"
-    images_list = [_file for _file in IMAGES_PATH.glob('*') if _file.is_file() and _file.suffix.lower() in IMAGE_SUFFIX]
+    images_list = [_file for _file in IMAGES_PATH.glob('**/*') if _file.is_file() and _file.suffix.lower() in IMAGE_SUFFIX]
 
     if not IMAGES_OUT_PATH.exists():
         IMAGES_OUT_PATH.mkdir()
@@ -20,8 +20,11 @@ def main():
         
         image_out = image.resize(SIZE)
         image_out = image_out.convert('RGB')
-        
-        image_out_path = IMAGES_OUT_PATH / image_path.with_suffix(".jpg").name
+
+        image_out_path = IMAGES_OUT_PATH / image_path.relative_to(IMAGES_PATH).with_suffix(".jpg")
+        if not image_out_path.parent.exists():
+            image_out_path.parent.mkdir(parents=True)
+
         image_out.save(image_out_path)
 
 
